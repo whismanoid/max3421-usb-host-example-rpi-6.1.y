d25d75405a06a150dc3c6aab804f626b346ca072
whismanoid <mark_underwood@sonic.net>
2023-07-07 00:03:44 -0700

    update test script

--- step5_test.sh
+++ step5_test.sh
@@ -8,19 +8,25 @@ printf "Test newly installed MAX3421-HCD USB host device driver\n"
 printf "\n"
 printf "uname -a\n"
 uname -a
+# $(uname -r) gives just the number triplet e.g. '6.1.37-v7+'
 #
 printf "\n"
 printf "dmesg | grep for max3421-hcd messages\n"
 # dmesg | grep -i 'max3421-hcd'
 # dmesg | grep -i 'max3421-hcd' | grep -i --invert-match 'bad rev'
-dmesg | grep --max-count=5 -i 'max3421-hcd'
-printf "\n"
-# skipping the dmesg "max3421-hcd spi0.1: bad rev 0x00"
-# ...which indicate that the hardware is not physically connected
-# ...to the MAX3421. Could be missing power, ground, spi, chip select.
-# TODO: squeeze out duplicate error lines of "max3421-hcd *: bad rev"
 if [[ ! -z $(dmesg | grep --max-count=1 -i 'max3421-hcd .*: bad rev') ]]; then
-	printf "FAIL: This dmesg error message aoubt max3421-hcd bd rev...\n"
+	dmesg | grep --max-count=5 -i 'max3421-hcd'
+	printf "PASS: dmesg log shows max3421-hcd module was loaded\n"
+	printf "\n"
+else
+	printf "FAIL: dmesg log has no max3421-hcd messages,\n"
+	printf "......which indicates max3421-hcd module was not loaded,\n"
+	printf "......or device tree overlay dtoverlay=spi0-max3421e failed.\n"
+	printf "\n"
+fi
+# search for dmesg "max3421-hcd spi0.1: bad rev 0x00"
+if [[ ! -z $(dmesg | grep --max-count=1 -i 'max3421-hcd .*: bad rev') ]]; then
+	printf "FAIL: This dmesg error message about max3421-hcd bd rev...\n"
     dmesg | grep --max-count=1 -i 'max3421-hcd .*: bad rev'
     printf "...indicates that the hardware is not physically connected\n"
     printf "to the MAX3421. Could be missing power, ground, spi, or chip select.\n"
@@ -33,8 +39,8 @@ if [[ -z $(lsusb -v --tree | grep -i 'max3421') ]]; then
 	printf "FAIL: expected a max3421-hcd root hub\n"
 	printf "\n"
 else
-	printf "PASS: found a max3421-hcd root hub\n"
 	lsusb -v --tree | grep -i 'max3421' --after-context=1
+	printf "PASS: found a max3421-hcd root hub\n"
 	printf "\n"
 fi
 printf "Use command lsusb -v --tree to view complete usb tree\n"
